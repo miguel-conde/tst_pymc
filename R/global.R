681e00641c0d379f4251073f926fdab12c5b0bfa
miguelco2000 <miguelco2000@gmail.com>
2023-09-25 07:30:44 +0200

    1st commit

---
+++ R/global.R
@@ -0,0 +1,9 @@
+DIR_DATA <- here::here('data')
+
+DIR_RAW_DATA <- file.path(DIR_DATA, 'raw')
+DIR_CLEAN_DATA <- file.path(DIR_DATA, 'clean')
+
+DIR_OUTPUTS <- here::here('outputs')
+
+DIR_OUTPUTS_REPORTS <- file.path(DIR_OUTPUTS, 'reports')
+DIR_OUTPUTS_FILES <- file.path(DIR_OUTPUTS, 'files')
