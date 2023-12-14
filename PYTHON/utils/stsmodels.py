042bcf496ed587cd65de506cf4c026efcb2108db
miguelco2000 <miguelco2000@gmail.com>
2023-12-14 07:34:32 +0100

    utils con funciones sts y prueba de model builder

--- PYTHON/utils/stsmodels.py
+++ PYTHON/utils/stsmodels.py
@@ -1,5 +1,152 @@
+import pandas as pd
+import numpy as np
+import pymc as pm
+
+from typing import Union, Dict
 from pymc_experimental.model_builder import ModelBuilder
 
 
+class StochasticTrend(ModelBuilder):
+    # Give the model a name
+    _model_type = "StochasticTrendModel"
+
+    # And a version
+    version = "0.1"
+
+    def build_model(self, X: pd.DataFrame, y: pd.Series, **kwargs):
+        """
+        build_model creates the PyMC model
+
+        Parameters:
+        model_config: dictionary
+            it is a dictionary with all the parameters that we need in our model example:  a_loc, a_scale, b_loc
+        X : pd.DataFrame
+            The input data that is going to be used in the model. This should be a DataFrame
+            containing the features (predictors) for the model. For efficiency reasons, it should
+            only contain the necessary data columns, not the entire available dataset, as this
+            will be encoded into the data used to recreate the model.
+
+        y : pd.Series
+            The target data for the model. This should be a Series representing the output
+            or dependent variable for the model.
+
+        kwargs : dict
+            Additional keyword arguments that may be used for model configuration.
+        """
+        # Check the type of X and y and adjust access accordingly
+        X_values = X["t"].values
+        y_values = y.values if isinstance(y, pd.Series) else y
+        self._generate_and_preprocess_model_data(X_values, y_values)
+
+        with pm.Model(coords=self.model_coords, check_bounds=True) as self.model:
+            # Create mutable data containers
+            x_data = pm.MutableData("x_data", X_values)
+            y_data = pm.MutableData("y_data", y_values)
+
+            # prior parameters
+            sigma_local_trend_lam_prior = self.model_config.get("sigma_local_trend_lam_prior", 1.0)
+            sigma_local_level_lam_prior = self.model_config.get("sigma_local_level_lam_prior", 1.0)
+            sigma_lam_prior = self.model_config.get("sigma_lam_prior", 1.0)
+
+            # priors
+            sigma_local_trend = pm.Exponential("sigma_local_trend", lam=sigma_local_trend_lam_prior)
+            sigma_local_level = pm.Exponential("sigma_local_level", lam=sigma_local_level_lam_prior)
+            sigma             = pm.Exponential("sigma", lam=sigma_lam_prior)             
+
+            local_trend_0    = pm.Normal("local_trend_0", mu=0, sigma=1e-1)
+            diff_local_trend = pm.Normal("diff_local_trend", mu=0, sigma=sigma_local_trend, dims="n_obs")
+            local_trend      = pm.Deterministic("local_trend", diff_local_trend.cumsum() + local_trend_0, dims="n_obs")
+
+            local_level_0    = pm.Normal("local_level_0", mu=0, sigma=.5)
+            diff_local_level = pm.Normal("diff_local_level", mu=local_trend, sigma=sigma_local_level, dims="n_obs")
+            local_level      = pm.Deterministic("local_level", diff_local_level.cumsum() + local_level_0, dims="n_obs")
+
+            y_obs = pm.Normal("y", mu=local_level, sigma=sigma, dims="n_obs", observed=y)
+
+    def _data_setter(
+        self, X: Union[pd.DataFrame, np.ndarray], y: Union[pd.Series, np.ndarray] = None
+    ):
+        if isinstance(X, pd.DataFrame):
+            x_values = X["t"].values
+        else:
+            # Assuming "t" is the first column
+            x_values = X[:, 0]
+
+        with self.model:
+            pm.set_data({"x_data": x_values})
+            if y is not None:
+                pm.set_data({"y_data": y.values if isinstance(y, pd.Series) else y})
+
+    @staticmethod
+    def get_default_model_config() -> Dict:
+        """
+        Returns a class default config dict for model builder if no model_config is provided on class initialization.
+        The model config dict is generally used to specify the prior values we want to build the model with.
+        It supports more complex data structures like lists, dictionaries, etc.
+        It will be passed to the class instance on initialization, in case the user doesn't provide any model_config of their own.
+        """
+        model_config: Dict = {
+            "sigma_local_trend_lam_prior": 1.0,
+            "sigma_local_level_lam_prior": 1.0,
+            "sigma_lam_prior": 1.0,
+        }
+        return model_config
+
+    @staticmethod
+    def get_default_sampler_config() -> Dict:
+        """
+        Returns a class default sampler dict for model builder if no sampler_config is provided on class initialization.
+        The sampler config dict is used to send parameters to the sampler .
+        It will be used during fitting in case the user doesn't provide any sampler_config of their own.
+        """
+        sampler_config: Dict = {
+            "draws": 1_000,
+            "tune": 1_000,
+            "chains": 3,
+            "target_accept": 0.95,
+        }
+        return sampler_config
+
+    @property
+    def output_var(self):
+        return "y"
+
+    @property
+    def _serializable_model_config(self) -> Dict[str, Union[int, float, Dict]]:
+        """
+        _serializable_model_config is a property that returns a dictionary with all the model parameters that we want to save.
+        as some of the data structures are not json serializable, we need to convert them to json serializable objects.
+        Some models will need them, others can just define them to return the model_config.
+        """
+        return self.model_config
+
+    def _save_input_params(self, idata) -> None:
+        """
+        Saves any additional model parameters (other than the dataset) to the idata object.
+
+        These parameters are stored within `idata.attrs` using keys that correspond to the parameter names.
+        If you don't need to store any extra parameters, you can leave this method unimplemented.
+
+        Example:
+            For saving customer IDs provided as an 'customer_ids' input to the model:
+            self.customer_ids = customer_ids.values #this line is done outside of the function, preferably at the initialization of the model object.
+            idata.attrs["customer_ids"] = json.dumps(self.customer_ids.tolist())  # Convert numpy array to a JSON-serializable list.
+        """
+        pass
+
+        pass
+
+    def _generate_and_preprocess_model_data(
+        self, X: Union[pd.DataFrame, pd.Series], y: Union[pd.Series, np.ndarray]
+    ) -> None:
+        """
+        Depending on the model, we might need to preprocess the data before fitting the model.
+        all required preprocessing and conditional assignments should be defined here.
+        """
+        self.model_coords = coords = {'n_obs': np.arange(X.shape[0])}  # in our case we're not using coords, but if we were, we would define them here, or later on in the function, if extracting them from the data.
+        # as we don't do any data preprocessing, we just assign the data given by the user. Note that it's a very basic model,
+        # and usually we would need to do some preprocessing, or generate the coords from the data.
+        self.X = X
+        self.y = y    
 
 
--- PYTHON/utils/ststtools.py
+++ PYTHON/utils/ststtools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def stochastic_trend(
         rng, 
